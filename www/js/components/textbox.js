5baa0b9c95d3a9b1f3b5a421700bc7ba26c794b0
rprashanth <prashanth11311@gmail.com>
2016-09-13 12:27:38 +0530

    get files

--- www/js/components/textbox.js
+++ www/js/components/textbox.js
@@ -7,6 +7,25 @@ var TextBox = React.createClass({displayName:'TextBox',
 	},
 	_onClick: function() {
 		console.log('here')
+		var fileArray = []
+		var fileList = document.getElementById('file')
+		console.log(fileList.files)
+
+		if ('files' in fileList){
+			
+			for(i = 0; i < fileList.files.length ;i++){
+				var file = {}
+				file['fileName'] = fileList['files'][i]['name']
+				file['fileSize'] = fileList['files'][i]['size']
+				file['fileType'] = fileList['files'][i]['type']
+
+				fileArray.push(file)
+			}
+		}
+		console.log(fileArray)
+		// use cordovas window.requestFileSystem method to get the local file system
+		//get the file names and use cordova's file tranfer methods
+		// send files to server
 	},
 	render: function(){
 		return(
@@ -18,12 +37,13 @@ var TextBox = React.createClass({displayName:'TextBox',
 						)
 					),
 				React.createElement('div', {className: 'col-sm-4'},
-					React.createElement('input', {type: 'file', multiple: 'multiple', name: 'file'})
+					React.createElement('input', {id: 'file', type: 'file', multiple: 'multiple', name: 'file'})
 					)
 				),
 				React.createElement('div', {className: 'row'},
 					React.createElement('div', {className: 'col-sm-4'},
-						React.createElement('input', {type: 'submit', className: 'uploadBtn', value: 'upload', onClick: this._onClick})
+						React.createElement('button', {type: 'button', className: 'btn btn-default', value: 'upload', onClick: this._onClick},
+							React.createElement('span', {className: 'glyphicon glyphicon-upload'}))
 					)
 				)
 			)
